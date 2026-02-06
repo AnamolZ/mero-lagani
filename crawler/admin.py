ec374a8c82a9829b9d9467b067cd7bef30bc60cb
AnamolZ <anmoldkl971@gmail.com>
2026-02-06 14:11:55 +0545

    feat: Register IPO model with Django admin, configuring list display, search, and filters.

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IPO
 
 # Register your models here.
+@admin.register(IPO)
+class IPOAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'share_type', 'share_group', 'issue_date')
+    search_fields = ('company_name', 'share_type')
+    list_filter = ('share_type', 'share_group')
